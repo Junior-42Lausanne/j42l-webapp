ae2b132d7e0ffe9787fd05fb257185900f63226a
Eric Rabbath <e.rabbath@gmail.com>
2025-08-05 09:56:22 +0200

    Extract UserProfile component

--- src/app/authenticated/profile/page.tsx
+++ src/app/authenticated/profile/page.tsx
@@ -1,5 +1,6 @@
 import PageTitle from "@/components/shared/page-title";
 import { Button } from "@/components/ui/button";
+import UserProfile from "@/components/users/user-profile";
 import { File, Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 
@@ -12,55 +13,7 @@ export default function ProfilePage() {
 				</Button>
 			</PageTitle>
 
-			<div className="flex items-center gap-8">
-				<div className="size-16 rounded-full border border-gray-700 bg-gray-300"></div>
-				<div className="flex-1">
-					<h2 className="text-xl font-bold">John Doe</h2>
-					<p className="text-sm text-gray-700 italic">
-						Ecole 42 Lausanne
-					</p>
-				</div>
-			</div>
-			<h2 className="mt-4 text-lg font-semibold">Presentation</h2>
-			<div>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae
-				saepe laudantium deleniti, delectus iusto obcaecati at autem ad
-				iste officiis aliquam dolores itaque molestiae? Qui at fugiat
-				culpa reiciendis animi.
-			</div>
-
-			<div className="grid grid-cols-2">
-				<div>
-					<h2 className="mt-4 text-lg font-semibold">Socials</h2>
-					<ul>
-						<li className="mt-2 flex cursor-pointer items-center gap-1 text-blue-700">
-							<Linkedin className="size-4" />
-							LinkedIn
-						</li>
-						<li className="mt-2 flex cursor-pointer items-center gap-1 text-blue-700">
-							<Github className="size-4" />
-							GitHub
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h2 className="mt-4 text-lg font-semibold">Documents</h2>
-					<ul>
-						<li className="mt-2 flex cursor-pointer items-center gap-1 text-blue-700">
-							<File className="size-4" />
-							CV
-						</li>
-						<li className="mt-2 flex cursor-pointer items-center gap-1 text-blue-700">
-							<File className="size-4" />
-							Lettre de recommandation XYZ
-						</li>
-						<li className="mt-2 flex cursor-pointer items-center gap-1 text-blue-700">
-							<File className="size-4" />
-							Certificat Kubernetes
-						</li>
-					</ul>
-				</div>
-			</div>
+			<UserProfile />
 		</div>
 	);
 }
