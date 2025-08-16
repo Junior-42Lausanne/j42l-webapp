38f857c97ca374ac1f9bcd31aa8aa518be0d4c09
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-08-16 12:19:35 +0200

    move route.ts to appropriate folder

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/features/auth/auth"; // path to your auth file
+import { auth } from "@/features/auth/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(auth);
