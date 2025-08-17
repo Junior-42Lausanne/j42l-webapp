735f3426aee2fb79932f7323f2b19a777b818711
Eric Rabbath <e.rabbath@gmail.com>
2025-08-17 16:22:13 +0200

    Add signout button to header and redirect from authenticated pages to login on signout

--- src/app/(authenticated)/projects/page.tsx
+++ src/app/(authenticated)/projects/page.tsx
@@ -2,9 +2,19 @@ import Pagination from '@/components/Pagination';
 import ProjectList from '@/features/projects/components/project-list';
 import ProjectSearch from '@/features/projects/components/project-search';
 import PageTitle from '@/components/page-title';
-import { LayoutDashboard } from 'lucide-react';
+import { auth } from '@/features/auth/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-const ProjectListPage = () => {
+const ProjectListPage = async () => {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session) {
+		redirect('/');
+	}
+
 	return (
 		<div>
 			<PageTitle title="Projects" />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,25 @@
+import { signOut } from '@/features/auth/actions/actions';
+import Link from 'next/link';
+
 const Header = () => {
 	return (
 		<div className="flex border-b border-solid border-black pb-5 pt-5">
-			<a className='m-auto' href="#">Job board</a>
-			<a className='m-auto' href="#">My applications</a>
-			<a className='m-auto' href="#">Profile</a>
+			<Link className="m-auto" href="/projects">
+				Job board
+			</Link>
+			<Link className="m-auto" href="#">
+				My applications
+			</Link>
+			<Link className="m-auto" href="#">
+				Profile
+			</Link>
+			<form action={signOut} className="m-auto">
+				<button className="cursor-pointer w-full h-full">
+					Sign out
+				</button>
+			</form>
 		</div>
-	)
+	);
 };
 
 export default Header;
