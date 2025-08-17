68c43c40cc02b8b70969a509192858371e2cb4c7
Yokio9 <dmatayi@gmail.com>
2025-08-17 13:30:14 +0200

    routes for project details pages are dynamically created

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,5 +1,20 @@
-const ProjectDetailsPage = () => {
-	return <div>Project details page</div>;
-};
+import ProjectDetailsInfo from '@/features/projects-details/components/project-details-info';
+import ProjectFullDescription from '@/features/projects-details/components/project-full-description';
+import ProjectPosition from '@/features/projects-details/components/project-positions';
 
-export default ProjectDetailsPage;
+export default async function ProjectDetailsPage({
+	params,
+}: {
+	params: Promise<{ projectId: string }>
+}) {
+	const { projectId } = await params
+	return (
+		<>
+			<ProjectDetailsInfo projectName={projectId}/>
+			<ProjectFullDescription />
+			<p className='text-xl mb-3 font-bold'>Positions</p>
+			<ProjectPosition />
+			<ProjectPosition />
+		</>
+	)
+}
--- src/features/projects-details/components/project-details-info.tsx
+++ src/features/projects-details/components/project-details-info.tsx
@@ -1,10 +1,10 @@
 import { User } from 'lucide-react';
 
-const ProjectDetailsInfo = () => {
+const ProjectDetailsInfo = ({projectName} : {projectName: string}) => {
 	return (
 		<div className='mt-10 mb-5 flex items-center'>
 			<User className='mr-8 w-15 h-15 rounded-full border'></User>
-			<p className='text-2xl font-bold'>Name</p>
+			<p className='text-2xl font-bold'>Project {projectName}</p>
 		</div>
 	)
 }
