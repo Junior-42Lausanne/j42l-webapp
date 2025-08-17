3a595fa297bf2d842447ab0b4acc87f258421b39
Yokio9 <dmatayi@gmail.com>
2025-08-17 11:14:11 +0200

    input labels created

---
+++ src/app/profile/edit/page.tsx
@@ -0,0 +1,11 @@
+import InputWithLabel from "@/features/users/components/profile-card";
+
+const EditProfile = () => {
+	return (
+		<div>
+			<InputWithLabel />
+			<InputWithLabel />
+		</div>
+	);
+};
+export default EditProfile;
