c913bef9564e83142b82ee02de1d7c71c0ff0b3a
Yokio9 <dmatayi@gmail.com>
2025-08-17 13:49:20 +0200

    added header for authenticated pages

--- src/app/(authenticated)/projects/page.tsx
+++ src/app/(authenticated)/projects/page.tsx
@@ -1,12 +1,10 @@
 import Pagination from '@/components/Pagination';
 import ProjectList from '@/features/projects/components/project-list';
 import ProjectSearch from '@/features/projects/components/project-search';
-import Header from '@/components/header';
 
 const ProjectListPage = () => {
 	return (
 		<div>
-			<Header />
 			<ProjectSearch />
 			<ProjectList />
 			<Pagination />
